1890626483259bce876c43baf99108f61be8eabf
Thomas T <327724@cognizant.com>
2020-03-21 17:16:55 +0530

    changes to App.java

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -1,62 +1,31 @@
-import io.pivotal.pal.tracker.gradlebuild.DependenciesGraphPlugin
+package io.pivotal.pal.tracker.timesheets;
 
-buildscript {
-        ext {
-        springBootVersion = "2.2.2.RELEASE"
-        springVersion = "5.0.9.RELEASE"
-        mysqlVersion = "8.0.13"
-        jacksonVersion = "2.9.7"
-        slf4jVersion = "1.7.25"
-        mockitoVersion = "2.23.0"
-        assertJVersion = "3.11.1"
-        hikariVersion = "3.1.0"
-        logbackVersion = "1.2.3"
-        junitVersion = "4.12"
-        okhttpVersion = "3.12.0"
-        jsonPathVersion = "2.4.0"
-        springCloudVersion = "Hoxton.RELEASE"
-        springCloudServicesClientLibrariesVersion = "2.2.1.RELEASE"
-        springCloudCommons = "2.0.0.RELEASE"
-        }
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestOperations;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-        repositories {
-        mavenCentral()
-        jcenter()
-        }
+import java.util.TimeZone;
 
-        dependencies {
-        classpath "org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion"
-        classpath "mysql:mysql-connector-java:$mysqlVersion"
-        }
-        }
 
-        apply plugin: DependenciesGraphPlugin
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
+public class App {
 
-        subprojects {
-        group "io.pivotal.pal.tracker"
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
 
-        apply plugin: "java"
-        defaultTasks "clean", "build"
-
-        repositories {
-        mavenCentral()
-        jcenter()
-        }
-
-        dependencies {
-        compile "com.fasterxml.jackson.core:jackson-core:$jacksonVersion"
-        compile "com.fasterxml.jackson.core:jackson-databind:$jacksonVersion"
-        compile "com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion"
-        compile "org.slf4j:slf4j-api:$slf4jVersion"
-
-        testCompile "junit:junit:$junitVersion"
-        testCompile "org.mockito:mockito-core:$mockitoVersion"
-        testCompile "org.assertj:assertj-core:$assertJVersion"
-        }
-
-        test {
-        testLogging {
-        exceptionFormat = 'full'
-        }
-        }
-        }
+    @Bean
+    ProjectClient projectClient(
+            RestOperations restOperations,
+            @Value("${registration.server.endpoint}") String registrationEndpoint
+    ) {
+        return new ProjectClient(restOperations, registrationEndpoint);
+    }
+}
